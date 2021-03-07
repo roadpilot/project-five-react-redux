1b530d97f0cc58f3b06430aa026700868c0e43a4
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-03-07 12:35:39 -0700

    login route to sessions create

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # resources :games
   # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/api/v1/login", to: "sessions#create"
   namespace :api do
     namespace :v1 do
       resources :users
