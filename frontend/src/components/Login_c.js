2d68c5eed45ab39f55a0f75258855f8f35445d1f
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-03-07 12:49:49 -0700

    stub login form component

---
+++ frontend/src/components/Login_c.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Login = () => {
+  const handleInputChange = event => {
+      return "foo"
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+  }
+
+  return (
+    <form 
+    onSubmit={handleSubmit}
+    >
+      <input 
+      placeholder="username" 
+      name="username" 
+      type="text" 
+      onChange={handleInputChange} 
+      />
+      <input 
+      placeholder="password" 
+      name="password" 
+      type="text" 
+      onChange={handleInputChange} 
+      />
+      <input 
+      type="submit" 
+      value="Log In"
+      />
+    </form>
+  )
+}
+
