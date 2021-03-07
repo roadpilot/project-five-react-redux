3ddbe5309144526d74abc08677be9ae744bd941f
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-03-07 13:34:46 -0700

    fix route for post vs get

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # resources :games
   # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/api/v1/login", to: "api/v1/sessions#create"
+  post "/api/v1/login", to: "api/v1/sessions#create"
   namespace :api do
     namespace :v1 do
       resources :users
