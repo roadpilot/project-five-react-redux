815f19eec069d121b4d637f8bf57f2395787347a
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-03-07 15:01:23 -0700

    remove initialstate values for user

--- frontend/src/reducers/user.js
+++ frontend/src/reducers/user.js
@@ -1,8 +1,10 @@
 //@@INIT returns default state of all reducers
-const initialState = [{
-    "username":"mda",
-    "name":"Martin"
-}]
+// const initialState = [{
+//     "username":"mda",
+//     "name":"Martin"
+// }]
+
+const initialState = []
 
 export default (state=initialState, action) => {
     switch (action.type){
