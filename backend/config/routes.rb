7ebc224b2501215559ae5c43ca98a04df6c341f2
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-03-07 10:11:13 -0700

    namespace routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :bets
-  resources :games
-  resources :users
+  # resources :bets
+  # resources :games
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  endend
