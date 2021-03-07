482c18b0bb52150f516f811ed786362a0a89d6bd
roadpilot <mdacht777@gmail.com>
2021-03-07 08:51:11 -0700

    seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Game.destroy_all
+Bet.destroy_all
+
+a=User.create(name: "Martin", username: "mda", password: "password")
+b=a.games.create(gameid: 25290)
+c=b.bets.create(bet_type: "spread", amount:1000, bet_status: "placed")
