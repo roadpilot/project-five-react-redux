51eb745448c757e196042686eb826c8fd95f3e57
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-03-07 17:11:21 -0700

    test build navbar

--- frontend/src/components/Navbar_c.js
+++ frontend/src/components/Navbar_c.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Navbar = () => {
+
+const Navbar = ({ currentUser, loggedIn }) => {
   return (
-    <div className="Navbar">
-        Navbar!
+    <div className="NavBar">
+      <NavLink exact activeClassName="active" to="/"  >Home</NavLink>
+      <NavLink exact activeClassName="active" to="/games"  >My Games</NavLink>
+      <NavLink exact activeClassName="active" to="/bets"  >My Bets</NavLink>
+      <NavLink exact activeClassName="active" to="/bets/new" >New Bet</NavLink>
+      { loggedIn ? <><p id="loggedin">Logged in as {currentUser.attributes.name}</p>Logout</> : null}
     </div>
   )
 }
 
-export default Navbar
+const mapStateToProps = state => {
+  return {
+    currentUser: state.session,
+    loggedIn: state.session===null
+  }
+}
+
+export default connect(mapStateToProps)(Navbar)
