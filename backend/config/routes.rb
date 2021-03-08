8d4df7ac5567f3c96c90fbcd35ced88e3b15e5c5
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-03-07 17:42:46 -0700

    add delete route to sessions

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/api/v1/login", to: "api/v1/sessions#create"
+  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
   get "/api/v1/current_user", to: "api/v1/sessions#current_user"
   namespace :api do
     namespace :v1 do
