144cb1b5f2d3180792a8dc51ed6c299225eb3c04
roadpilot <65299501+roadpilot@users.noreply.github.com>
2021-03-08 17:28:16 -0700

    and signup route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post "/api/v1/login", to: "api/v1/sessions#create"
   delete "/api/v1/logout", to: "api/v1/sessions#destroy"
   get "/api/v1/current_user", to: "api/v1/sessions#current_user"
+  post "/api/v1/signup", to: "api/v1/users#create"
   namespace :api do
     namespace :v1 do
       resources :users
