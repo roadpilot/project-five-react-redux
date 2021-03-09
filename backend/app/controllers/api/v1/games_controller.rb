4381b5ad36210c92be89a7108c44b149d74e8399
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-03-09 07:34:26 -0700

    create method for games controller

--- backend/app/controllers/api/v1/games_controller.rb
+++ backend/app/controllers/api/v1/games_controller.rb
@@ -1,4 +1,20 @@
 class Api::V1::GamesController < ApplicationController
-    def create
+  def create
+    @game = current_user.games.build(game_params)
+
+    if @game.save
+      render json:  @game, status: :created
+    else
+      error_resp = {
+        error: @game.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Only allow a trusted parameter "white list" through.
+    def game_params
+      params.require(:game).permit(:gameid)
     end
 end
