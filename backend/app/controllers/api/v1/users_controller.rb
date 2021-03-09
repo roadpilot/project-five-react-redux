d651728ff070d477a18166f940fdf0b7b574e880
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-03-09 05:15:59 -0700

    users controller create method

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,24 @@ class Api::V1::UsersController < ApplicationController
 
     render json: @users
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user), status: :created
+    else
+      resp = {
+        error: @user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
+  end
+
+  private
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.require(:user).permit(:name, :username, :password)
+  end
 end
+
