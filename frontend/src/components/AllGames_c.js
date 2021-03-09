862bfa6c7a6d5c6de4a32bd31778ade0b038704d
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-03-09 06:29:54 -0700

    AllGames component

--- frontend/src/components/AllGames_c.js
+++ frontend/src/components/AllGames_c.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import GameCard from './GameCard_c'
+import { connect } from 'react-redux'
+import { addGame } from '../actions/allGames_a'
 
-const AllGames = () => {
-  return (
-    <div className="AllGames">
-      <GameCard/>
-    </div>
-  )
+const AllGames = props => {
+    let games = props.allGames
+    let statuses = ['canceled']
+    games = games.filter(function (el) {
+      return (!statuses.includes(el.status))
+    });
+
+  const gameCards = props.allGames.length > 0 
+  ?
+    games.map(game => {
+      // console.log(game.gameId)
+      return (
+        <GameCard key={game.gameId} game_id={game.gameId} buttonText="Add" buttonHandler={props.addGame} game={props.allGames.find(game_find => game_find.gameId === game.gameId)} bets={[]}/>
+      )
+    })
+ :
+    null
+  return gameCards
 }
 
-export default AllGames
+const mapStateToProps = state => {
+  return {
+    allGames: state.allGames
+  }
+}
+
+export default connect(mapStateToProps,{addGame})(AllGames)
