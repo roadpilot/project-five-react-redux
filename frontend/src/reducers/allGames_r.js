495c7ca84e4bdcdb32262d7c739a215a3e30132c
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-03-09 06:03:53 -0700

    allGames reducer

---
+++ frontend/src/reducers/allGames_r.js
@@ -0,0 +1,22 @@
+const initialState = []
+
+export default (state = initialState, action) => {
+  // console.log("PRE SWITCH")
+  switch (action.type) {
+    case "SET_ALL_GAMES":
+      // console.log("SET",action.games)
+      return action.games
+    case "ADD_MY_GAME":
+      // console.log("HIT REDUCER",action.game)
+      return state.concat(action.game)
+    case "UPDATE_GAME":
+      return state.map(game => game.id === action.game.id ? action.game : game)
+    // case "DELETE_GAME":
+    //   //console.log( "action.gameId is ", action.gameId)
+    //   return state.filter(game => game.id === action.gameId ? false : true)
+    case "CLEAR_ALL_GAMES":
+      return initialState
+    default:
+      return state
+  }
+}
