47c4ac71b3a6013741d96d764ef4cb9744e63f71
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-03-10 08:57:16 -0700

    build MyGames component

--- frontend/src/components/MyGames_c.js
+++ frontend/src/components/MyGames_c.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import GameCard from './GameCard_c'
+import { dropGame } from '../actions/myGames_a'
 
-const MyGames = () => {
-  return (
-    <div className="MyGames">
-    MyGames
-    </div>
-  )
+const MyGames = props => {
+  const gameCards = props.myGames.length > 0 ?
+    props.myGames.map(t => {
+      return (
+        <GameCard key={t.id} game_id={t.id} buttonText="Drop" buttonHandler={props.dropGame} game={props.allGames.find(game => game.gameId === t.attributes.gameid)} bets={t.attributes.bets}/>
+      )}
+    ) :
+    null
+  return gameCards
 }
 
-export default MyGames
+const mapStateToProps = state => {
+  return {
+    allGames: state.allGames,
+    myGames: state.myGames
+  }
+}
+
+export default connect(mapStateToProps,{dropGame})(MyGames)
