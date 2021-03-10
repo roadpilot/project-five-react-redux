75cdd763a71601095995f5853c9877141b031bef
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-03-10 08:46:50 -0700

    stub and add MyGames component

--- frontend/src/components/MainContainer_c.js
+++ frontend/src/components/MainContainer_c.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import AllGames from './AllGames_c.js'
+import MyGames from './MyGames_c.js'
 
 const MainContainer = () => {
   return (
     <div className="MainContainer">
+      <MyGames/>
       <AllGames/>
     </div>
   )
---
+++ frontend/src/components/MyGames_c.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyGames = () => {
+  return (
+    <div className="MyGames">
+    MyGames
+    </div>
+  )
+}
+
+export default MyGames
