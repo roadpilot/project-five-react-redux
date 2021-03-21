bbeb8f7b2b9313090431f886f830c3bc635a2901
roadpilot <65299501+roadpilot@users.noreply.github.com>
2021-03-21 14:16:10 -0600

    Add margin to allgames

--- frontend/src/components/AllGames_c.js
+++ frontend/src/components/AllGames_c.js
@@ -123,7 +123,7 @@ const AllGames = props => {
     // </div>
  :
     null
-  return <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8">{gameCards}</div>
+  return <div className="m-5 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8">{gameCards}</div>
 }
 
 const mapStateToProps = state => {
