ca8e56c139b9fa878ac4c3e148e9edcfeee1cd0d
roadpilot <65299501+roadpilot@users.noreply.github.com>
2021-03-21 13:52:11 -0600

    Experiment with tailwind css

--- frontend/src/components/AllGames_c.js
+++ frontend/src/components/AllGames_c.js
@@ -32,28 +32,98 @@ const AllGames = props => {
   ?
     games.map(game => {
       // console.log(game.gameId)
+      let cardColor = "black"
       const gameTime = new Date(game.schedule.date).toLocaleString().replace(":00","").slice(11)
+      console.log(game.details.league)
+      switch(game.details.league) {
+        case "MLB":
+          cardColor = "indigo"
+          break;
+        case "NCAAB":
+          cardColor = "red"
+          break;
+        case "NBA":
+          cardColor = "green"
+          break;
+        case "NHL":
+          cardColor = "yellow"
+          break;
+        default:
+          cardColor = "black"
+      }
       const buttonStyle = {}
       return (
-        <div key={game.gameId} className="flex-container-list">
-          <div className="wrapper">
-            <header className="header-list">
-              {gameTime} {game.details.league}: {game.summary}
-            </header>
-            <aside className="aside aside-1">
-              <button 
-              style={buttonStyle}
+    //     <div key={game.gameId} className="flex-container-list">
+    //       <div className="wrapper">
+    //         <header className="header-list">
+    //           {gameTime} {game.details.league}: {game.summary}
+    //         </header>
+    //         <aside className="aside aside-1">
+    //           <button 
+    //           style={buttonStyle}
+    //           onClick={()=>props.addGame(game.gameId)}
+    //           >Add</button>
+    //         </aside>
+    //       </div>
+    //     </div>
+    //     // <GameCard key={game.gameId} game_id={game.gameId} buttonText="Add" buttonHandler={props.addGame} game={props.allGames.find(game_find => game_find.gameId === game.gameId)} bets={[]}/>
+            // <div className="grid grid-cols-3 gap-4">
+            <div
+              className={`px-5 py-3 rounded-lg transform transition bg-${cardColor}-500 hover:bg-${cardColor}-400 hover:-translate-y-0.5 focus:ring-${cardColor}-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 active:bg-${cardColor}-600 uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base`}
+              href="#"
               onClick={()=>props.addGame(game.gameId)}
-              >Add</button>
-            </aside>
-          </div>
-        </div>
-        // <GameCard key={game.gameId} game_id={game.gameId} buttonText="Add" buttonHandler={props.addGame} game={props.allGames.find(game_find => game_find.gameId === game.gameId)} bets={[]}/>
+            >
+            {gameTime} {game.details.league}: {game.summary}
+            </div>
+            // </div>
+            
+
+
       )
     })
+    // <div className="bg-gray-100 grid lg:grid-cols-2 2xl:grid-cols-5">
+    //   <div
+    //     className="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-2"
+    //   >
+    //     <div className="xl:max-w-xl">
+    //       <img className="h-10" src="/img/logo.svg" alt="Workcation" />
+    //       <img
+    //         className="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover object-center lg:hidden"
+    //         src="/img/beach-work.jpg"
+    //         alt="Woman workcationing on the beach"
+    //       />
+    //       <h1
+    //         className="mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl"
+    //       >
+    //         You can work from anywhere.
+    //         <br className="hidden lg:inline" />
+    //         <span className="text-indigo-500">Take advantage of it.</span>
+    //       </h1>
+    //       <p className="mt-2 text-gray-600 sm:mt-4 sm:text-xl">
+    //         Workcation helps you find work-friendly rentals in beautiful locations so you can enjoy
+    //         some nice weather even when you're not on vacation.
+    //       </p>
+    //       <div className="mt-4 sm:mt-6">
+    //         <a
+    //           className="inline-block px-5 py-3 rounded-lg transform transition bg-indigo-500 hover:bg-indigo-400 hover:-translate-y-0.5 focus:ring-indigo-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 active:bg-indigo-600 uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base"
+    //           href="#"
+    //         >
+    //           Book your escape
+    //         </a>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <div className="hidden relative lg:block 2xl:col-span-3">
+    //     <img
+    //       className="absolute inset-0 w-full h-full object-cover object-center"
+    //       src="/img/beach-work.jpg"
+    //       alt="Woman workcationing on the beach"
+    //     />
+    //   </div>
+    // </div>
  :
     null
-  return gameCards
+  return <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8">{gameCards}</div>
 }
 
 const mapStateToProps = state => {
