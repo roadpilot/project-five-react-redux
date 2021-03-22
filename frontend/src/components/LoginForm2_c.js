ab7ec858a71de059d2e7349effacf534d5e4f1ad
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-03-21 19:35:17 -0600

    Collect errors from return and display

--- frontend/src/components/LoginForm2_c.js
+++ frontend/src/components/LoginForm2_c.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { login } from '../actions/session_a'
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
 
 const LoginForm = ({ login }) => {
-  const handleInputChange = event => {
-      return "foo"
-  }
+  const [errorMsg, setErrorMsg] = useState('')
+  // const handleInputChange = event => {
+  //     return "foo"
+  // }
 
-  const handleSubmit = event => {
+  async function handleSubmit(event){
     event.preventDefault()
-    console.log(event.target)
-    login(event.target)
+    const errors = await login(event.target)
+    if (errors){setErrorMsg(errors)}
   }
 
   return (
@@ -23,17 +24,17 @@ const LoginForm = ({ login }) => {
         className='w-11/12 max-w-2xl mx-auto mt-8'
       >
         <h1 className='font-bold text-3xl mb-2'>Log In</h1>
-        <p className='h-8 text-red-400'>{/*this.props.state.errors.status.message*/}</p>
+        <p className='h-8 text-red-400'>{errorMsg}</p>
         <fieldset>
           <label className='block uppercase mb-2' htmlFor='email'>
             User Name:
           </label>
           <input
-            placeholder="username" 
+            placeholder="" 
             name="username" 
             type="text" 
             className='w-full border-2 focus:outline-none focus:ring-2 p-4 mb-4'
-            // onChange={event => handleChange(event)}
+            onChange={() => setErrorMsg('')}
             // value={this.props.formData.email}
           />
         </fieldset>
@@ -46,17 +47,26 @@ const LoginForm = ({ login }) => {
             name='password'
             id='password'
             className='w-full border-2 focus:outline-none focus:ring-2 p-4 mb-4'
-            // onChange={event => handleChange(event)}
+            onChange={() => setErrorMsg('')}
             // value={formData.password}
           />
         </fieldset>
         <input
-          className='w-full text-center uppercase p-4 bg-blue-300 cursor-pointer mt-4'
+          className=
+          {`w-full mt-4 px-5 py-3 rounded-lg transform transition bg-indigo-500 hover:bg-indigo-400 hover:-translate-y-0.5 focus:ring-indigo-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 active:bg-indigo-600 uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base`} 
           type='submit'
           value='Log In'
         />
+    <NavLink exact to="/signup"  >
+        <input
+          className=
+          {`w-full mt-4 px-5 py-3 rounded-lg transform transition bg-gray-500 hover:bg-gray-400 hover:-translate-y-0.5 focus:ring-gray-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 active:bg-gray-600 uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base`} 
+          type='button'
+          value="I'm a new user..."
+        />
+    
+    </NavLink>
         </form>
-    <NavLink exact activeClassName="w-full text-center" to="/signup"  >I'm a new user...</NavLink>
 </div>      
     </div>
   )
