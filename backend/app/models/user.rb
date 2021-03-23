0596f5f805f3349f6f09ef75141faacb42c48148
roadpilot <65299501+roadpilot@users.noreply.github.com>
2021-03-23 17:36:34 -0600

    Add User model validation

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :games
     has_many :bets, through: :games
+    validates :name, :username, presence: true
+    validates :username, uniqueness: true
 end
