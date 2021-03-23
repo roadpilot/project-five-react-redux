b1ea07d317d5b8b3d2f729661e628a43953ba35d
roadpilot <65299501+roadpilot@users.noreply.github.com>
2021-03-23 17:09:35 -0600

    Build handlesubmit and send error props to child component

--- frontend/src/components/StatefulFormParent_c.js
+++ frontend/src/components/StatefulFormParent_c.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Form from './StatefulForm_c'
 import DisplayData from './StatefulFormDisplayData_c'
+import { connect } from 'react-redux'
+import { signup } from '../actions/session_a'
  
 class ParentComponent extends React.Component {
   state = {
     username: "",
     password: "",
     name: "",
+    errors: "",
   }
   
 
@@ -16,11 +19,31 @@ class ParentComponent extends React.Component {
     })
     
   }
- 
-  handleSubmit = (event) => {
+
+  handleSubmit = event => {
     event.preventDefault()
-    console.log(this.state)
+    console.log(this.props)
+    this.props.signup(this.state)
+      .then(response => {
+        this.setState({
+          ...this.state, "errors": response
+        })
+      })
+      .then(response => {
+        console.log("STATE",this.state)
+      })
+    //   .catch(errors => {
+    //     console.log(errors)
+    // })
   }
+ 
+  // async handleSubmit(event){
+  //   event.preventDefault()
+  //   console.log(this.props)
+  //   // const errors = await this.props.signup(event.target)
+  //   // if (errors){return null}    
+  //   console.log(this.state)
+  // }
   
   render() {
     return (
@@ -29,6 +52,7 @@ class ParentComponent extends React.Component {
           formData={this.state}
           handleChange={this.handleChange}
           handleSubmit={this.handleSubmit}
+          errors={this.state.errors}
         />
         <DisplayData formData={this.state} />
       </div>
@@ -36,4 +60,11 @@ class ParentComponent extends React.Component {
   }
 }
  
-export default ParentComponent;
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     signup: (formData) => dispatch(signup(formData))
+//   }
+// }
+
+// export default ParentComponent;
+export default connect(null,{signup})(ParentComponent)
