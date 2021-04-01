0401183f1ca95973dbef3fe9ee3d8a4c2b8024c6
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-04-01 13:18:42 -0600

    Configure for local API

--- frontend/src/actions/allGames_a.js
+++ frontend/src/actions/allGames_a.js
@@ -20,7 +20,8 @@ export const addGameToState = game => {
 
 export const getAllGames = () => {
   return dispatch => {
-  fetch("http://104.255.169.179/~achterma/jfjf75h6bgjvhfhge/api.php", {
+  // fetch("http://104.255.169.179/~achterma/jfjf75h6bgjvhfhge/api.php", {
+  fetch("http://localhost/default.aspx", {
     "method": "GET",
     "headers": {
       // "x-rapidapi-key": "97c9af71cbmshedc491e264e8007p1a8b16jsn21e4bf6176ca",
