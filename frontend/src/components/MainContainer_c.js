91492d0b1b12ea853c7db57868bf2be34ee834e1
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-04-01 13:19:15 -0600

    Stub instructions div

--- frontend/src/components/MainContainer_c.js
+++ frontend/src/components/MainContainer_c.js
@@ -6,6 +6,7 @@ const MainContainer = (props) => {
   return (
     <div className="MainContainer pt-20">
       <MyGames/>
+      <div className="ml-20 width-full centered">Click any game below to add to your games...</div>
       <AllGames leagueFilter={props.leagueFilter}/>
     </div>
   )
