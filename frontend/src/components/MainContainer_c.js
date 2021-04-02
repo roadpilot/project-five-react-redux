029478d77789ac4d4b989e16af171af77fd51f01
mdacht777 <65299501+mdacht777@users.noreply.github.com>
2021-04-02 09:56:50 -0600

    Styling for instruction div

--- frontend/src/components/MainContainer_c.js
+++ frontend/src/components/MainContainer_c.js
@@ -6,7 +6,9 @@ const MainContainer = (props) => {
   return (
     <div className="MainContainer pt-20">
       <MyGames/>
-        <div className={`px-5 py-3 rounded-lg bg-gray-500 uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base text-center`}>
+        <div
+        className={`ml-5 px-5 py-3 text-center rounded-lg bg-gray-500 uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base`}
+        >
         Click any game below to add to your games...
         </div>
       <AllGames leagueFilter={props.leagueFilter}/>
