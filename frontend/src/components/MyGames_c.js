e883d338bf03fea47a69f38edb3d0c10af64065f
roadpilot <65299501+roadpilot@users.noreply.github.com>
2021-04-03 13:41:11 -0600

    Cleanup: remove console.logs

--- frontend/src/components/MyGames_c.js
+++ frontend/src/components/MyGames_c.js
@@ -7,7 +7,6 @@ const MyGames = props => {
     let games = props.allGames
 
     const mygameIds = props.myGames.map(mygame => mygame.attributes.gameid)
-    // console.log(mygameIds)
     games = games.filter(function (el) {
       return (mygameIds.includes(el.gameId))
     });
@@ -23,18 +22,11 @@ const MyGames = props => {
       }
       return 0;
     });
-
-    console.log("GAMEWS",games)
     
   const gameCards = games.length > 0 ?
     games.map(game => {
       const gc = props.myGames.find(g => game.gameId === g.attributes.gameid)
       const bets = gc.attributes.bets
-      console.log("GAME", game)
-      const gameTime = game ? new Date(game.schedule.date).toLocaleString().replace(":00","").slice(11) : null
-      // console.log(gc.attributes.gameid)
-      // console.log("BETS", bets)
-      // debugger
       return (
         <GameCard 
         key={gc.id} 
