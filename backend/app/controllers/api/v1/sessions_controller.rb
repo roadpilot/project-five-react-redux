1a3818f9766337d7a5dc6d95a876f190f4ad7d5f
roadpilot <65299501+roadpilot@users.noreply.github.com>
2021-04-03 17:08:55 -0600

    Return error when no user logged in

--- backend/app/controllers/api/v1/sessions_controller.rb
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -1,22 +1,28 @@
 class Api::V1::SessionsController < ApplicationController
 
-    def create
-        @user = User.find_by(username: params[:session][:username])
-        if @user && @user.authenticate(params[:session][:password])
-            session[:user_id] = @user.id
-            render json: UserSerializer.new(@user), status: :ok
-            # render json: @user
-        else
-            render json: {
-                error: "Invalid username or password.  Please try again."
-            }
-        end
-    end
+  def create
+      @user = User.find_by(username: params[:session][:username])
+      if @user && @user.authenticate(params[:session][:password])
+          session[:user_id] = @user.id
+          render json: UserSerializer.new(@user), status: :ok
+          # render json: @user
+      else
+          render json: {
+              error: "Invalid username or password.  Please try again."
+          }
+      end
+  end
 
-    def current_user
-        @user = User.find_by(id: session[:user_id])
+  def current_user
+      @user = User.find_by(id: session[:user_id])
+      if @user
         render json: UserSerializer.new(@user), status: :ok
-    end
+      else
+        render json: {
+            error: "You must be logged in."
+        }
+      end
+  end
 
   def destroy
     session.clear
