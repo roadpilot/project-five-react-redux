22d066fe9bfedd59c94110a06b09c6b677f8b083
roadpilot <65299501+roadpilot@users.noreply.github.com>
2021-04-03 14:24:33 -0600

    Cleanup: remove unused user reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,11 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
-import user from './reducers/user.js'
 import session from './reducers/session_r.js'
 import allGames from './reducers/allGames_r.js'
 import myGames from './reducers/myGames_r.js'
 
 // const user = () => []  //just for testing
 const reducer = combineReducers({
-  user,
   session,
   allGames,
   myGames
